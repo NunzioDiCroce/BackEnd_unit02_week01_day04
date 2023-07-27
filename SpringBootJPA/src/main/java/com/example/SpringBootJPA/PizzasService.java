69426026fe927787e7269bc442b08b882ed9db99
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-27 16:00:06 +0200

    Update PizzasService.java

--- SpringBootJPA/src/main/java/com/example/SpringBootJPA/PizzasService.java
+++ SpringBootJPA/src/main/java/com/example/SpringBootJPA/PizzasService.java
@@ -1,8 +1,12 @@
 package com.example.SpringBootJPA;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PizzasService {
+
+	@Autowired
+	private IPizzasRepository iPizzasRepository;
 
 }
