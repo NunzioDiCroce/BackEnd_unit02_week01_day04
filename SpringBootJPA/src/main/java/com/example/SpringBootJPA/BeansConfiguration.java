f806a8718c58e89cf8ed0c82d12735eb46f85b49
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-27 16:08:57 +0200

    Update BeansConfiguration.java

--- SpringBootJPA/src/main/java/com/example/SpringBootJPA/BeansConfiguration.java
+++ SpringBootJPA/src/main/java/com/example/SpringBootJPA/BeansConfiguration.java
@@ -1,5 +1,25 @@
 package com.example.SpringBootJPA;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+
 public class BeansConfiguration {
+
+	@Bean(name = "MargheritaPizza")
+	public Pizza getMargheritaPizza() {
+		return new Pizza("Margherita Pizza", "tomato", "cheese", 1104, 4.99);
+	}
+
+	@Bean(name = "HawaiianPizza")
+	public Pizza getHawaiianPizza() {
+		return new Pizza("Hawaiian Pizza", "tomato", "cheese", "ham", "pineapple", 1024, 6.49);
+	}
+
+	@Bean(name = "SalamiPizza")
+	public Pizza getSalamiPiazza() {
+		return new Pizza("Salami Pizza", "tomato", "cheese", "salami", 1160, 5.99);
+	}
 
 }
