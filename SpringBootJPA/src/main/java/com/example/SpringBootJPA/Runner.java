769e89e97811b7e0bd23d87bfefb49d28174510b
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-27 16:11:49 +0200

    Update Runner.java

--- SpringBootJPA/src/main/java/com/example/SpringBootJPA/Runner.java
+++ SpringBootJPA/src/main/java/com/example/SpringBootJPA/Runner.java
@@ -1,5 +1,18 @@
 package com.example.SpringBootJPA;
 
-public class Runner {
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+
+public class Runner implements CommandLineRunner {
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("Runner is running...");
+
+	}
 
 }
