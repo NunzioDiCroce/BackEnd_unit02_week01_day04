9e468b8815b9b5ecd74033830414c6338d34db65
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-27 16:03:29 +0200

    Update PizzasService.java

--- SpringBootJPA/src/main/java/com/example/SpringBootJPA/PizzasService.java
+++ SpringBootJPA/src/main/java/com/example/SpringBootJPA/PizzasService.java
@@ -3,10 +3,17 @@ package com.example.SpringBootJPA;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class PizzasService {
 
 	@Autowired
 	private IPizzasRepository iPizzasRepository;
 
+	public void save(Pizza _pizza) {
+		iPizzasRepository.save(_pizza);
+		log.info(_pizza.getName() + " salvata con successo");
+	}
 }
