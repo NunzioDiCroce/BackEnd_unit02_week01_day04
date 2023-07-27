fb0d917fa81ce107605fcd0564f86c3011ccdc3b
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-27 16:54:26 +0200

    Update Runner.java

--- SpringBootJPA/src/main/java/com/example/SpringBootJPA/BeansConfiguration.java
+++ SpringBootJPA/src/main/java/com/example/SpringBootJPA/BeansConfiguration.java
@@ -2,8 +2,11 @@ package com.example.SpringBootJPA;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+
+@PropertySource("classpath:application.properties")
 
 public class BeansConfiguration {
 
--- SpringBootJPA/src/main/java/com/example/SpringBootJPA/Runner.java
+++ SpringBootJPA/src/main/java/com/example/SpringBootJPA/Runner.java
@@ -1,6 +1,7 @@
 package com.example.SpringBootJPA;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
@@ -9,9 +10,24 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 
 public class Runner implements CommandLineRunner {
+
 	@Override
 	public void run(String... args) throws Exception {
 		log.info("Runner is running...");
+
+		configWithAnnotationConfig();
+
+	}
+
+	public static void configWithAnnotationConfig() {
+
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(SpringBootJpaApplication.class);
+
+		Pizza margherita = ctx.getBean("MargheritaPizza", Pizza.class);
+
+		PizzasService pizzaService = ctx.getBean(PizzasService.class);
+
+		pizzaService.save(margherita);
 
 	}
 
